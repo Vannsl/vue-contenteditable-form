072a311e2f2ab81f704675c1e74c9582c992aac2
Vannsl <boehner.vanessa@gmail.com>
2021-07-30 08:32:00 +0200

    change focus style of editable blocks

--- src/components/EditableForm.vue
+++ src/components/EditableForm.vue
@@ -41,7 +41,7 @@
         role="button"
         tabindex="0"
       >
-        <div data-action="handle" class="p-1 cursor-move text-gray-400">
+        <div data-action="handle" class="cursor-move text-gray-400">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -60,7 +60,7 @@
         <EditableBlock
           :tag="element.tag"
           :html="element.html"
-          class="w-full"
+          class="w-full px-2 py-1 focus:outline-none focus:bg-gray-100"
           @changeContent="(html) => updateBlock(element.id, html)"
           @enterPressed="addAndFocusOnBlock(index)"
         />
